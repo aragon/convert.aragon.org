fe6a82332638592f4ef521818b0924ed031aff1f
Pierre Bertet <hello@pierre.world>
2020-03-24 18:45:49 +0000

    Next.js tweaks (#2)

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import * as Sentry from '@sentry/browser'
 import App from 'next/app'
 import Head from 'next/head'
 import { Router } from 'components/Router'
 import Navbar from 'components/Navbar'
-import { Web3ConnectProvider } from '../lib/web3-connect'
+import { Web3ConnectProvider } from 'lib/web3-connect'
+import env from 'lib/environment'
 
 import '../public/app.css'
+
+if (env('SENTRY_DSN')) {
+  Sentry.init({
+    dsn: env('SENTRY_DSN'),
+    environment: env('NODE_ENV'),
+    release: 'anj.aragon.org@' + env('BUILD'),
+  })
+}
 
 export default class extends App {
   render() {
