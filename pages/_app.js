c9ba1afc0834482c3a80e18751130f4684108730
Pierre Bertet <hello@pierre.world>
2020-03-25 23:11:00 +0000

    app.css cleanup, move styles in styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,10 @@ import React from 'react'
 import * as Sentry from '@sentry/browser'
 import App from 'next/app'
 import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 import Navbar from 'components/Navbar'
 import { Web3ConnectProvider } from 'lib/web3-connect'
 import env from 'lib/environment'
-
-import '../public/app.css'
 
 if (env('SENTRY_DSN')) {
   Sentry.init({
@@ -16,6 +15,24 @@ if (env('SENTRY_DSN')) {
   })
 }
 
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Manrope';
+    src: url('/fonts/ManropeGX.ttf');
+  }
+  body,
+  button {
+    font-family: 'Manrope', sans-serif;
+  }
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+    overflow-y: hidden;
+    font-size: 16px;
+  }
+`
+
 export default class extends App {
   render() {
     const { Component, pageProps } = this.props
@@ -24,6 +41,7 @@ export default class extends App {
         <Head>
           <title>Aragon Court</title>
         </Head>
+        <GlobalStyles />
         <Web3ConnectProvider>
           <Component {...pageProps} />
         </Web3ConnectProvider>
