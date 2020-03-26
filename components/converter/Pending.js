ed5cfb4230c10c6bc8959fdcee60e69bf82274f5
Pierre Bertet <hello@pierre.world>
2020-03-26 12:02:10 +0000

    Remove styled-components imports

--- components/converter/Pending.js
+++ components/converter/Pending.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 import processing from './assets/loader.gif'
 
 function Pending({ isFinal }) {
