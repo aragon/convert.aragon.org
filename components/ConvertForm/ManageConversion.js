e14e79aa725db1f6b22cef9ec2c4dc513fe53a83
andy-hook <hello@andy-hook.co.uk>
2020-05-22 11:03:03 +0100

    Cleanup step creation on unmount or rerender

--- components/ConvertForm/ManageConversion.js
+++ components/ConvertForm/ManageConversion.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   useOpenOrder,
   useClaimOrder,
@@ -16,74 +16,85 @@ function ManageConversion({ toAnj, fromAmount, toAmount, handleReturnHome }) {
   const getAllowance = useAllowance()
   const [conversionSteps, setConversionSteps] = useState()
 
-  // Interacting with the bonding curve involves 2, 3 or 4 transactions (depending on the direction and state of allowance):
-  // 1. Reset approval (If we're converting ANT -> ANJ, an allowance was previously set but abandoned)
-  // 2. Raise approval (If we're converting ANT -> ANJ, the current allowance is not high enough)
-  // 3. Open a buy order
-  // 4. Claim the order
-  const createConvertSteps = useCallback(async () => {
-    let openOrderHash
-    let steps = []
+  useEffect(() => {
+    let cancelled = false
 
-    // First we check for allowance if the direction is ANT -> ANJ
-    if (toAnj) {
-      const allowance = await getAllowance()
+    // Interacting with the bonding curve involves 2, 3 or 4 transactions (depending on the direction and state of allowance):
+    // 1. Reset approval (If we're converting ANT -> ANJ, an allowance was previously set but abandoned)
+    // 2. Raise approval (If we're converting ANT -> ANJ, the current allowance is not high enough)
+    // 3. Open a buy order
+    // 4. Claim the order
+    const createConvertSteps = async () => {
+      let openOrderHash
+      let steps = []
 
-      // and if we need more, add a step to ask for an approval
-      if (allowance.lt(bigNum(fromAmount))) {
-        steps.unshift([
-          'Raise approval',
-          {
-            createTx: () => changeAllowance(fromAmount),
-          },
-        ])
+      // First we check for allowance if the direction is ANT -> ANJ
+      if (toAnj) {
+        const allowance = await getAllowance()
 
-        // Then there's the case when a user has an allowance set to the market maker contract
-        // but wants to convert even more tokens this time. When dealing with this case
-        // we want to first prepend a transaction to reset the allowance back to zero
-        // (before raising it in the next transaction from above).
-        if (!allowance.isZero()) {
+        // Bail out early if component is unmounted before resolving
+        if (cancelled) {
+          return
+        }
+
+        // and if we need more, add a step to ask for an approval
+        if (allowance.lt(bigNum(fromAmount))) {
           steps.unshift([
-            'Reset approval',
+            'Raise approval',
             {
-              createTx: () => changeAllowance(0),
+              createTx: () => changeAllowance(fromAmount),
             },
           ])
+
+          // Then there's the case when a user has an allowance set to the market maker contract
+          // but wants to convert even more tokens this time. When dealing with this case
+          // we want to first prepend a transaction to reset the allowance back to zero
+          // (before raising it in the next transaction from above).
+          if (!allowance.isZero()) {
+            steps.unshift([
+              'Reset approval',
+              {
+                createTx: () => changeAllowance(0),
+              },
+            ])
+          }
         }
       }
+
+      // Next add the open order
+      steps.push([
+        `Create ${toAnj ? 'buy' : 'sell'} order`,
+        {
+          createTx: () => openOrder(fromAmount, toAnj),
+
+          // We need to store a reference to the hash so we can use it in the following step
+          hashCreated: hash => {
+            openOrderHash = hash
+          },
+        },
+      ])
+
+      // And finally the claim order
+      steps.push([
+        'Claim order',
+        {
+          createTx: () => claimOrder(openOrderHash, toAnj),
+        },
+      ])
+
+      // Update state to reflect the correct amount of steps
+      // Show loader for a small amount of time to provide a smooth visual experience
+      setTimeout(() => {
+        setConversionSteps(steps)
+      }, 900)
     }
 
-    // Next add the open order
-    steps.push([
-      `Create ${toAnj ? 'buy' : 'sell'} order`,
-      {
-        createTx: () => openOrder(fromAmount, toAnj),
-
-        // We need to store a reference to the hash so we can use it in the following step
-        hashCreated: hash => {
-          openOrderHash = hash
-        },
-      },
-    ])
-
-    // And finally the claim order
-    steps.push([
-      'Claim order',
-      {
-        createTx: () => claimOrder(openOrderHash, toAnj),
-      },
-    ])
-
-    // Update state to reflect the correct amount of steps
-    // Show loader for a small amount of time to provide a smooth visual experience
-    setTimeout(() => {
-      setConversionSteps(steps)
-    }, 900)
-  }, [claimOrder, fromAmount, getAllowance, openOrder, toAnj, changeAllowance])
-
-  useEffect(() => {
     createConvertSteps()
-  }, [createConvertSteps])
+
+    return () => {
+      cancelled = true
+    }
+  }, [changeAllowance, claimOrder, fromAmount, getAllowance, openOrder, toAnj])
 
   return (
     <>
