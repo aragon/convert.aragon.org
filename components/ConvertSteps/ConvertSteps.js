39a8acfd14779049df99507349a69f2f677b1eff
andy-hook <hello@andy-hook.co.uk>
2020-06-03 01:56:48 +0100

    Hoist step state above presentation components

--- components/ConvertSteps/ConvertSteps.js
+++ components/ConvertSteps/ConvertSteps.js
@@ -1,4 +1,10 @@
-import React, { useCallback, useState, useEffect, useRef } from 'react'
+import React, {
+  useCallback,
+  useState,
+  useEffect,
+  useRef,
+  useReducer,
+} from 'react'
 import useMeasure from 'react-use-measure'
 import { ResizeObserver as Polyfill } from '@juggle/resize-observer'
 import PropTypes from 'prop-types'
@@ -6,30 +12,106 @@ import {
   STEPPER_IN_PROGRESS,
   STEPPER_SUCCESS,
   STEPPER_ERROR,
+  STEP_WAITING,
+  STEP_WORKING,
+  STEP_SUCCESS,
+  STEP_ERROR,
 } from './stepper-statuses'
 import Divider from './Divider'
-import ManageStep from './ManageStep'
+import Step from './Step'
 import StepperLayout from './StepperLayout'
 import StepperTitle from './StepperTitle'
+
+function buildInitialStepState(steps) {
+  return steps.map(() => {
+    return {
+      status: STEP_WAITING,
+      hash: null,
+      active: false,
+    }
+  })
+}
 
 function ConvertSteps({ toAnj, fromAmount, toAmount, onReturnHome, steps }) {
   const [stepperStatus, setStepperStatus] = useState(STEPPER_IN_PROGRESS)
   const [stepperStage, setStepperStage] = useState(0)
-  const [retryingStep, setRetryingStep] = useState(null)
   const [stepperDisplayMode, setStepperDisplayMode] = useState('large')
   const [innerBoundsWidth, setInnerBoundsWidth] = useState()
   const [outerBoundsRef, outerBounds] = useMeasure({ scroll: true, Polyfill })
   const innerBoundsRef = useRef(null)
 
+  function reduceSteps(steps, [action, stepIndex, value]) {
+    if (action === 'setActive') {
+      steps[stepIndex].active = true
+      return [...steps]
+    }
+    if (action === 'setHash') {
+      steps[stepIndex].hash = value
+      return [...steps]
+    }
+    if (action === 'setStatus') {
+      steps[stepIndex].status = value
+      return [...steps]
+    }
+    return steps
+  }
+
+  const [stepsState, updateStep] = useReducer(
+    reduceSteps,
+    buildInitialStepState(steps)
+  )
+
+  const attemptStepSigning = useCallback(
+    async stepIndex => {
+      const onHashCreated = steps[stepIndex][1].hashCreated
+      const createTx = steps[stepIndex][1].createTx
+
+      try {
+        updateStep(['setActive', stepIndex])
+        updateStep(['setStatus', stepIndex, STEP_WAITING])
+
+        // Awaiting confirmation
+        const transaction = await createTx()
+
+        onHashCreated && onHashCreated(transaction.hash)
+        updateStep(['setHash', stepIndex, transaction.hash])
+
+        // Mining transaction
+        updateStep(['setStatus', stepIndex, STEP_WORKING])
+        await transaction.wait()
+
+        // Success
+        updateStep(['setStatus', stepIndex, STEP_SUCCESS])
+
+        // Activate next step if not last
+        if (stepperStage < steps.length - 1) {
+          setStepperStage(stepperStage + 1)
+        }
+
+        // Show overall stepper success if final step succeeds
+        if (stepperStage === steps.length - 1) {
+          setStepperStatus(STEPPER_SUCCESS)
+        }
+      } catch (err) {
+        // If there's a problem mining the transaction we catch it
+        // and visually feedback to the user
+        updateStep(['setStatus', stepIndex, STEP_ERROR])
+        setStepperStatus(STEPPER_ERROR)
+        console.error(err)
+      }
+    },
+    [steps, stepperStage]
+  )
+
   const handleRetry = useCallback(() => {
     setStepperStatus(STEPPER_IN_PROGRESS)
-    setRetryingStep(stepperStage)
-  }, [stepperStage])
 
-  const handleError = useCallback(() => {
-    setRetryingStep(null)
-    setStepperStatus(STEPPER_ERROR)
-  }, [])
+    attemptStepSigning(stepperStage)
+  }, [attemptStepSigning, stepperStage])
+
+  useEffect(() => {
+    attemptStepSigning(stepperStage)
+  }, [stepperStage, attemptStepSigning])
 
   useEffect(() => {
     if (!innerBoundsWidth) {
@@ -51,40 +133,17 @@ function ConvertSteps({ toAnj, fromAmount, toAmount, onReturnHome, steps }) {
     }
   }, [outerBounds.width, innerBoundsWidth, stepperDisplayMode])
 
-  const handleSuccess = useCallback(
-    optionalCb => {
-      return () => {
-        setRetryingStep(null)
-        optionalCb && optionalCb()
-
-        // Activate next step if not last
-        if (stepperStage < steps.length - 1) {
-          setStepperStage(stepperStage + 1)
-        }
-
-        // Show overall stepper success if final step succeeds
-        if (stepperStage === steps.length - 1) {
-          setStepperStatus(STEPPER_SUCCESS)
-        }
-      }
-    },
-    [stepperStage, steps]
-  )
-
   const renderSteps = mode => {
     return steps.map((step, index) => {
       const stepCurrentlyActive = stepperStage === steps.indexOf(step)
 
       const stepToRender = (
-        <ManageStep
+        <Step
           title={step[0]}
           number={index + 1}
-          canRetry={retryingStep === index}
-          handleTx={step[1].createTx}
-          active={stepCurrentlyActive}
-          onHashCreation={step[1].hashCreated}
-          onSuccess={handleSuccess(step[1].success)}
-          onError={handleError}
+          active={stepsState[index].active}
+          status={stepsState[index].status}
+          transactionHash={stepsState[index].hash}
         />
       )
 
